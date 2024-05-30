3c27950f3d3542c7e74febfa814f9d3c519aa71c
dgmarra <dgamarra@continental.edu.pe>
2024-05-30 18:49:31 -0500

    Caso 4: Calcular promedio con un conjunto de n elementos (Azul)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -2,14 +2,11 @@ class Conjunto:
     def __init__(self, conjunto):
         self.conjunto = conjunto
     def promedio(self):
-        if len(self.conjunto) == 1:
-            return (self.conjunto[0])
-        elif len(self.conjunto) == 2:
-            return (self.conjunto[0] + self.conjunto[1]) / 2
-        elif len(self.conjunto) > 2:
+        if len(self.conjunto) > 0:
             return sum(self.conjunto) / len(self.conjunto)
         else:
             return None
+
 
 
 
