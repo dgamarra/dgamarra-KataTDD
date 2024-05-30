1004d657f4cd3337731c07122171eb74665877cc
dgmarra <dgamarra@continental.edu.pe>
2024-05-30 18:43:14 -0500

    Caso 3: Calcular promedio con un conjunto de dos elementos (Verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -4,6 +4,9 @@ class Conjunto:
     def promedio(self):
         if len(self.conjunto) == 1:
             return (self.conjunto[0])
+        elif len(self.conjunto) == 2:
+            return (self.conjunto[0] + self.conjunto[1]) / 2
         else:
             return None
+
 
